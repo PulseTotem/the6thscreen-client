6957b51a89819b3bff9b4b3a8fa24ae7fc1f67aa
Christian Brel <ch.brel@gmail.com>
2015-06-01 16:46:25 +0200

    Updated for prod URLs.

--- app/scripts/core/Constants.ts
+++ app/scripts/core/Constants.ts
@@ -4,9 +4,9 @@
  */
 
 class Constants {
-	static BACKEND_URL : string = "http://localhost:4000/clients";
-	//static BACKEND_URL : string = "http://t6s-backend.herokuapp.com/clients";
+	//static BACKEND_URL : string = "http://localhost:4000/clients";
+	static BACKEND_URL : string = "http://t6s-backend.herokuapp.com/clients";
 
-	static SOURCES_SERVER_URL : string = "http://localhost:5000/calls";
-	//static SOURCES_SERVER_URL : string = "http://t6s-sourcesserver.herokuapp.com/calls";
+	//static SOURCES_SERVER_URL : string = "http://localhost:5000/calls";
+	static SOURCES_SERVER_URL : string = "http://t6s-sourcesserver.herokuapp.com/calls";
 }
